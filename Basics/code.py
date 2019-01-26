71c52236403fc6c960bde1e98d7435f6904653f3
Hirak Bhayani <hirak.bhayani@gmail.com>
2019-01-26 13:11:13 +0530

    Code file created.

---
+++ Basics/code.py
@@ -0,0 +1,52 @@
+# --------------
+# Code starts here
+class_1 = ['Geoffrey Hinton','Andrew Ng','Sebastian Raschka','Yoshua Bengio']
+class_2 = ['Hilary Mason','Carla Gentry','Corinna Cortes']
+new_class = class_1 + class_2
+print(new_class)
+new_class.append('Peter Warden')
+print(new_class)
+new_class.remove('Carla Gentry')
+print(new_class)
+# Code ends here
+
+
+# --------------
+# Code starts here
+courses = {'Math':65,'English':70,'History':80,'French':70,'Science':60}
+print(courses)
+total = (sum(courses.values()))
+print(total)
+percentage = (total/500)*100
+print("The percentage scored by Geoffrey Hinton is",percentage)
+# Code ends here
+
+
+# --------------
+# Code starts here
+mathematics = {'Geoffrey Hinton':78,'Andrew Ng':95,'Sebastian Raschka':65,'Yoshua Benjio':50,'Hilary Mason':70,'Corinna Cortes':66,'Peter Warden':75}
+print(mathematics)
+topper = max(mathematics,key=mathematics.get)
+print("The maximum marks in Mathematics were scored by",topper)
+
+
+
+# Code ends here  
+
+
+# --------------
+# Given string
+
+# Code starts here
+topper = 'andrew ng'
+first_name = (topper.split()[0])
+print("The topper's first name is",first_name)
+last_name = (topper.split()[1])
+print("The topper's last name is",last_name)
+full_name = last_name+" "+first_name
+print("The topper's full name is",full_name)
+certificate_name = full_name.upper()
+print(certificate_name)
+# Code ends here
+
+
